f682a1109791c9e5b7dee69b893ca2ea7c553969
Gonzalo <grupo8edix@gmail.com>
2023-04-21 21:36:53 +0200

    Codigo Start with Retrofit

--- app/src/main/java/com/mealmuse/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/mealmuse/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -1,19 +1,20 @@
-package com.mealmuse
+package com.mealmuse.ui.fragments.favorites
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mealmuse.R
 
-class FoodJokeFragment : Fragment() {
+class FavoriteRecipesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_joke, container, false)
+        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
     }
 
 }
